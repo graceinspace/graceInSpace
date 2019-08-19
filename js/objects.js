2bbface7c701d2d9d80bf6053ed5b58d410153e3
Eva E-P <eva.emendoerfer@gmail.com>
2019-08-19 15:49:55 -0400

    rendering objects through map and SingleObj component works. Created function that generates random coordinates

---
+++ js/objects.js
@@ -0,0 +1,28 @@
+const allObjects = [
+  {
+    source: require("./res/beachball.obj"),
+    resources: [require("./res/beachball.mtl")],
+    type: "OBJ"
+  },
+  {
+    source: require("./res/sunglasses.obj"),
+    resources: [require("./res/sunglasses.mtl")],
+    type: "OBJ"
+  },
+  {
+    source: require("./res/wine.obj"),
+    resources: [require("./res/wine.mtl")],
+    type: "OBJ"
+  },
+  {
+    source: require("./res/bitcoin.obj"),
+    resources: [require("./res/bitcoin.mtl")],
+    type: "OBJ"
+  },
+  {
+    source: require("./res/duckie.obj"),
+    resources: [require("./res/duckie.mtl")],
+    type: "OBJ"
+  }
+];
+export default allObjects;
