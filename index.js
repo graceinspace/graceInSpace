77955af9f105153deecb20ad03c3edfaa308fa96
Claire Filipek <cfilipek@sva.edu>
2019-08-23 17:04:25 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { AppRegistry } from "react-native";
 import GraceInSpace from "./App.js";
-import { store } from "./js/store";
-import React, { Component } from "react";
-import { Provider } from "react-redux";
+
 
 AppRegistry.registerComponent("GraceInSpace", () => GraceInSpace);
 
--- js/GameLostScreen.js
+++ js/GameLostScreen.js
@@ -1,89 +1,80 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Text,
   View,
   StyleSheet,
   TouchableHighlight,
-  Button,
-  Image
-} from "react-native";
-// import WelcomeScreen from "./WelcomeScreen";
-// import { Provider } from "react-redux";
-// import store from "./store/index";
-import { changeToUnset, changeToSpace } from "./store/gameActions";
-import {connect} from "react-redux"
+  Image,
+} from 'react-native';
+import { changeToUnset, changeToSpace } from './store/gameActions';
+import { connect } from 'react-redux';
 
 class GameLostScreen extends Component {
   constructor() {
     super();
-    // this.state = {
-    //   startAgain: false
-    // };
   }
 
-  // changeState = () => {
-  //   this.setState({ startAgain: true });
-  // };
-  // Replace this function with the contents of _getVRNavigator() or _getARNavigator()
-  // if you are building a specific type of experience.
   render() {
-    // if (this.state.startAgain === false) {
-      return (
-        <View style={localStyles.container}>
+    return (
+      <View style={localStyles.container}>
         <Text
-                  style={{
-                    fontFamily: "Futura-CondensedExtraBold",
-                    color: "white",
-                    textAlign: "center",
-                    fontSize: 50
-                  }}
-                >
-                  Game over
-                </Text>
-                <Image style={{ width: 300, height: 240 }}
-              source={{uri: 'https://i.imgur.com/piIoTYo.png'}}/>
-          <Text style={{ color: "white", textAlign: "center", paddingBottom: 20, fontSize: 20 }}>
-            Ouch! You ran out of time!
-          </Text>
-          <TouchableHighlight
-                    style={localStyles.buttons}
-                    onPress={() => {
-                     this.props.changeToSpace();
-                     }}
-                    underlayColor={"#68a0ff"}>
-           <Text style={localStyles.buttonText}>Try Again!</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-                    style={localStyles.buttons}
-                    onPress={() => {
-                     this.props.changeToUnset();
-                     }}
-                    underlayColor={"#68a0ff"}>
-           <Text style={localStyles.buttonText}>Back to Home!</Text>
-          </TouchableHighlight>
-        </View>
-      );
-     }
-    // } else if (this.state.startAgain === true) {
-    //   return (
-    //     <Provider store={store}>
-    //       <WelcomeScreen />
-    //     </Provider>
-    //   );
+          style={{
+            fontFamily: 'Futura-CondensedExtraBold',
+            color: 'white',
+            textAlign: 'center',
+            fontSize: 50,
+          }}
+        >
+          Game over
+        </Text>
+        <Image
+          style={{ width: 300, height: 240 }}
+          source={{ uri: 'https://i.imgur.com/piIoTYo.png' }}
+        />
+        <Text
+          style={{
+            color: 'white',
+            textAlign: 'center',
+            paddingBottom: 20,
+            fontSize: 20,
+          }}
+        >
+          Ouch! You ran out of time!
+        </Text>
+        <TouchableHighlight
+          style={localStyles.buttons}
+          onPress={() => {
+            this.props.changeToSpace();
+          }}
+          underlayColor={'#68a0ff'}
+        >
+          <Text style={localStyles.buttonText}>Try Again!</Text>
+        </TouchableHighlight>
+        <TouchableHighlight
+          style={localStyles.buttons}
+          onPress={() => {
+            this.props.changeToUnset();
+          }}
+          underlayColor={'#68a0ff'}
+        >
+          <Text style={localStyles.buttonText}>Back to Home!</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 var localStyles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   buttonText: {
-    // marginTop: 5,
-    color: "white",
-    textAlign: "center",
-    fontSize: 20
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 20,
   },
   buttons: {
     height: 50,
@@ -92,18 +83,18 @@ var localStyles = StyleSheet.create({
     paddingBottom: 10,
     marginTop: 5,
     marginBottom: 15,
-    backgroundColor: "#4AC7CB",
-    borderRadius: 10
-  }
+    backgroundColor: '#4AC7CB',
+    borderRadius: 10,
+  },
 });
 
 const mapStateToProps = state => ({
-  score: state.score
+  score: state.score,
 });
 
 const mapDispatchToProps = dispatch => ({
   changeToUnset: () => dispatch(changeToUnset()),
-  changeToSpace: () => dispatch(changeToSpace())
+  changeToSpace: () => dispatch(changeToSpace()),
 });
 
 module.exports = connect(
