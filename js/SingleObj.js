33d0cb532b21d8eb00c56859f7efef2bb99beb05
lkobetz <larissa.kobetz@gmail.com>
2019-08-26 15:55:38 -0400

    added animation!

--- js/SingleObj.js
+++ js/SingleObj.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Viro3DObject } from "react-viro";
+import { Viro3DObject, ViroAnimations } from "react-viro";
 import { connect } from "react-redux";
 import { upCount } from "./store/gameActions";
 import store from "./store/index";
@@ -20,6 +20,11 @@ class SingleObj extends Component {
   b = this.rando();
   c = this.rando();
 
+  getAnimation() {
+    let num = this.rando();
+    return num % 2 === 0 ? "moveRight" : "moveLeft";
+  }
+
   render() {
     return (
       <Provider store={store}>
@@ -36,11 +41,22 @@ class SingleObj extends Component {
             this.setState({ contains: false });
             this.props.scoreUp();
           }}
+          animation={{
+            name: this.getAnimation(),
+            run: true,
+            loop: true,
+            easing: "EaseIn"
+          }}
         />
       </Provider>
     );
   }
 }
+
+ViroAnimations.registerAnimations({
+  moveRight: { properties: { positionX: "+=5" }, duration: 3000 },
+  moveLeft: { properties: { positionX: "-=5" }, duration: 3000 }
+});
 
 const mapStateToProps = state => ({
   score: state.score,
